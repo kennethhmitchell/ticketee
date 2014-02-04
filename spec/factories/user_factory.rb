553b006ccab64896e7ce39f786d0235fe5c1ea9d
kennethhmitchell <kennethhmitchell@gmail.com>
2014-02-04 13:45:10 -0500

    Proper implement of signing in

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
  factory :user do
   name "Example user"
+  email "sample@example.com"
+  password "password"
+  password_confirmation "password"
  end
 end
 
---
+++ spec/factories/user_factory.rb~
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+ factory :user do
+  name "Example user"
+ end
+end
+
